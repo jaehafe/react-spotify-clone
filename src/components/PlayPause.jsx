9b30995fab8aa64224f299cabdd966ccb7062a5f
Jaeha Lee <wogkdkrm112@gmail.com>
2022-10-25 03:10:42 +0900

    second commit

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,3 +1,21 @@
-const PlayPause = () => <div>Loader</div>;
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+import React from 'react';
+
+const PlayPause = ({
+  isPlaying,
+  activeSong,
+  song,
+  handlePause,
+  handlePlay,
+}) => (
+  <div>
+    {isPlaying && activeSong?.title === song.title ? (
+      <FaPauseCircle size={35} className="text-gray-300" onClick={handlePlay} />
+    ) : (
+      <FaPlayCircle size={35} className="text-gray-300" onClick={handlePlay} />
+    )}
+  </div>
+);
 
 export default PlayPause;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import playerReducer from './features/playerSlice';
+import { shazamCoreApi } from './services/shazamCore';
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
